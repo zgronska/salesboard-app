fa11bd79df1f6fc19267c6e9cefa4890cd4ce38f
zgronska <zgronska@gmail.com>
2023-03-14 13:35:45 +0100

    Selected HTML elements + improved formatting

--- index.js
+++ index.js
@@ -1,178 +1,42 @@
-// Product A info
-let productA = {
-    emoji: "⭐",
-    revenue: 200,
-    commision: 50
-}
+//* Select HTML elements
 
-// Product B info
-let productB = {
-    emoji: "🔥",
-    revenue: 300,
-    commision: 75
-}
+//? Main app
 
+const petName = document.getElementById("pet-name"); // Pet name heading
+const petPic = document.getElementById("pet-pic"); // Pet picture
 
-/*NOTES:
-1. This is a first draft of the JS algorithm and logic for our app. 
-Let me know if you think this looks clear, or if we should change the work flow of the functions etc.
-2. Some html changes (and CSS?) will be required,
- and I added a rough list for them in the end of the document.
-3. First part is for algorithm and logic, based on what the app would roughly do. 
-I tried to make it malleable, so if we want to add features, it would be easy to do. 
-But I'm not sure if I was able to take everything into account, so suggestions are welcome :)
-4. Second part is for specifying a few core concepts, and what they are supposed to do in this first draft. 
- We might want to change some of it for the next draft.
-5. I added some other comments along the way too.
-6. I didn't add the localStorage yet! I've had some issues getting it to work before, 
-so I need to brush up on my knowledge on it first...
- */
+// Action buttons
+const feedBtn = document.getElementById("feed-btn");
+const playBtn = document.getElementById("play-btn");
+const groomBtn = document.getElementById("groom-btn");
 
-/* ALGORITHM & LOGIC
+// Cards - to be filled with values
+const petCareEl = document.getElementById("pet-care"); // Pet care
+const achievementsEl = document.getElementById("achievements"); // Achievements
+const scoreEl = document.getElementById("score"); // Score
+const levelEl = document.getElementById("level"); // Level
 
-renderLayoutOne() 
+//? Choose your pet
 
-renderLayoutOne(){ /*this would be the "choose your pet"-section 
+const cypSection = document.getElementById("cyp-section");
 
-	render the pop-up-window visible with css.style
+// Pet type radio buttons
+const cypDog = document.getElementById("cyp-dog");
+const cypCat = document.getElementById("cyp-cat");
 
-	OR
+// Pet name input
+const cypName = document.getElementById("cyp-name");
 
-	chooseYourPetWindow.innerHtml = `
-	<form>
-		<input type="radio" id="choice-cat">
-		<input type="radio" id="choice-dog">
-		<input type="text" id="pet-name">
-		<button type="submit" id="pet-name-btn">
-	</form>
-	`
-}
+const cypSendBtn = document.getElementById("cyp-sendbtn"); // Send button
 
-chooseYourPetForm.addEventListener("submit", function(e){
-		- get the FormData
-		- render the pet-name into Main App
-		- render the pet-image into Main App
-		- hide the pop-up-form-window
-})
+//? Pop up
 
-.container.addEventListener("click", function(e){	
-	IF (e.target.id === "feed-btn"){
-		addToPetCareArray("food")
-	} ELSE IF (e.target.id === "play-btn"){
-		addToPetCareArray("play")
-	} ELSE IF (e.target.id === "groom-btn"){
-		addToPetCareArray("groom")
-	} ELSE IF ((e.target.id === "reset-btn"){
-		resetGame()
-	}
-})
+const achievementEmoji = document.getElementById("achievement-emoji"); // Span element for emoji
 
-addToPetCareArray(itemId){	
-	- get the target item from itemsArray
-	- push the target item into petCareArray
+const popupBtn = document.getElementById("btn-popup"); // Close popup button
 
-	- run renderPetCareArray(petCareArray)
-	- run achievementsArray(petCareArray)
-}
+//? COMMENTS
+//? We probably won't need all of these, but I wrote it all out just in case. After we're done, let's delete the unused ids from HTML. We should also move them to local functions as we write them for better practice, as in general elements should not be accessed globally if they are not needed.
 
-renderPetCareArray(array){
-	 - loop throught the array:
-	 - render the emojis,
-	 - add scores to totalScore 
-}
-
-achievementsArray(array){
-	IF (1st interaction && "choice-cat.checked"){
-	 	push "🐈" to achievementsArray
-	} else IF (1st interaction && "choice-dog.checked"){
-	add "🐕" to achievementsArray
-	}
-
-	IF (totalScore === 10 || totalScore === 20 || totalScore === 30){
-		push "💝" to Achievements
-		add 1 to petLevel
-		renderAchievementUnlocked()
-	}
-}
-
-renderAchievementUnlocked(){
-	pop-up-window for the achievements
-	and a button for closing the window
-}
-
-resetGame(){
-	◦ clear Local Storage
-	◦ clear all added data
-	◦ renderLayoutOne()
-
-
-}
-
-*/
-
-/*  CORE VARIABLES ETC:
-
-buttons:
-
-feed-btn:
-	◦ push "🍖" to petCareArray
-	◦ add "points" to totalScore
-	◦ run renderPetCare()
-play-btn:
-	◦ push "⚽" to petCareArray
-	◦ add "points" to totalScore
-	◦ run renderPetCare()
-groom-btn:
-	◦ push "🧼" to petCareArray
-	◦ add "points" to totalScore
-	◦ run renderPetCare()
-
-reset-btn
-	◦ clear perCareArray and achievementsArray
-	◦ clear totalScore and PetLevel
-	◦ clear Local Storage
-	◦ render()
-	toggle-theme-btn
-	◦ css.styles: change color scheme
-
-arrays:
-
-itemsArray
-	◦ to hold all the emoji-items
-	◦ OR DO WE WANT TO HAVE SEPARATE ARRAYS FOR PET CARE ITEMS AND ACHIEVEMENT ITEMS?
-
-petCareArray
-	◦ which will be rendered into html
-
-achievementsArray
-	◦ which will be rendered into html
-
-
-
-array-objects:
-
-	petCareItems = {
-		emoji: "🍖",
-		id: "food",
-		score: 1
-	}
-	achievementItems = {
-		emoji: "🐕",
-		id: "dog",
-	}
-
-other variables:
- 
-const totalScore
-• will be calculated and  rendered
-
-const petLevel
-• will be calculated and rendered
-*/
-
-/*  HTML CHANGES:
--do we want to add the "choose your pet"-section straight into the html (and hide it with css), or should we render it with JS (innerHtml)?
--add toggle and reset buttons in Main App
--add pet name field in Main App
--add Achievements Unlocked -section
-*/
+//TODO: Add event listeners to the buttons
+//code here
